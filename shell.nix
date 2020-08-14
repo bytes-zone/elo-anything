2dfcdad5e0d2f6b79aa8706d664914263cac8c36
Brian Hicks <brian@brianthicks.com>
2020-08-14 13:56:01 -0500

    nixify

---
+++ shell.nix
@@ -0,0 +1,24 @@
+{ ... }:
+let
+  sources = import ./nix/sources.nix;
+
+  nixpkgs = import sources.nixpkgs { };
+
+  niv = import sources.niv { };
+in with nixpkgs;
+stdenv.mkDerivation {
+  name = "elo-anything";
+  buildInputs = [
+    niv.niv
+    git
+
+    # Elm
+    elmPackages.elm
+    elmPackages.elm-format
+    elmPackages.elm-test
+
+    # Build Stuff
+    modd
+    devd
+  ];
+}
