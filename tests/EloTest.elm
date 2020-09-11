29612aaa9ca8c47f166825f9459e9207b7e1172c
Brian Hicks <brian@brianthicks.com>
2020-09-10 22:57:19 -0500

    describe expectations of score differentials

--- tests/EloTest.elm
+++ tests/EloTest.elm
@@ -1,6 +1,6 @@
 module EloTest exposing (..)
 
-import Elo
+import Elo exposing (..)
 import Expect exposing (FloatingPointTolerance(..))
 import Fuzz exposing (Fuzzer)
 import Test exposing (..)
@@ -32,10 +32,32 @@ oddsTest =
     describe "odds"
         [ fuzz ratingFuzzer "given equal ratings, the outcome is 50/50" <|
             \rating ->
-                Elo.odds rating rating
+                odds rating rating
                     |> Expect.within (Relative 0.001) 0.5
         , fuzz inequalRatingsFuzzer "given inequal ratings, the outcome favors the higher of the two ratings" <|
             \( a, b ) ->
-                Elo.odds (max a b) (min a b)
+                odds (max a b) (min a b)
                     |> Expect.greaterThan 0.5
+        ]
+
+
+newRating : Test
+newRating =
+    describe "newRating"
+        [ fuzz ratingFuzzer "a player gains more points by winning against a heavy favorite than a relative equal" <|
+            \player ->
+                let
+                    equal =
+                        player + 100
+
+                    favorite =
+                        player + 1000
+
+                    ( newAgainstEqual, _ ) =
+                        Elo.newRating Elo.sensitiveKFactor player WonAgainst equal
+
+                    ( newAgainstFavorite, _ ) =
+                        Elo.newRating Elo.sensitiveKFactor player WonAgainst favorite
+                in
+                newAgainstEqual |> Expect.lessThan newAgainstFavorite
         ]
