9c32bc8b3b8515a210f89663a307633c0d896f03
Brian Hicks <brian@brianthicks.com>
2020-09-10 22:38:38 -0500

    test compatibility with the old format

--- tests/LeagueTest.elm
+++ tests/LeagueTest.elm
@@ -4,7 +4,9 @@ import Dict exposing (Dict)
 import Expect
 import Fuzz exposing (Fuzzer)
 import Json.Decode as Decode
+import Json.Encode as Encode
 import League exposing (League)
+import Player
 import PlayerTest exposing (playerFuzzer)
 import Test exposing (..)
 
@@ -15,6 +17,12 @@ decoderTests =
         [ fuzz leagueFuzzer "encode and decode are symmetrical" <|
             \league ->
                 League.encode league
+                    |> Decode.decodeValue League.decoder
+                    |> Expect.equal (Ok league)
+        , fuzz leagueFuzzer "is backwards-compatible with the older dictionary format" <|
+            \league ->
+                league.players
+                    |> Encode.dict identity Player.encode
                     |> Decode.decodeValue League.decoder
                     |> Expect.equal (Ok league)
         ]
