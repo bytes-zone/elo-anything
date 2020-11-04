486e5698f0d76b221d71f4250ecc668ba2ee7b3b
Brian Hicks <brian@brianthicks.com>
2020-11-04 11:06:15 -0600

    lift elo-anything to an attrset

--- default.nix
+++ default.nix
@@ -3,26 +3,27 @@ let
   sources = import nix/sources.nix;
   nixpkgs = import sources.nixpkgs { };
   gitignore = import sources.gitignore { };
-in with nixpkgs;
-stdenv.mkDerivation {
-  name = "elo-anything";
-  src = gitignore.gitignoreSource ./.;
+in with nixpkgs; {
+  elo-anything = stdenv.mkDerivation {
+    name = "elo-anything";
+    src = gitignore.gitignoreSource ./.;
 
-  buildInputs = [ elmPackages.elm elmPackages.elm-test ];
-  buildPhase = pkgs.elmPackages.fetchElmDeps {
-    elmPackages = import ./nix/elm-srcs.nix;
-    elmVersion = "0.19.1";
-    registryDat = ./nix/registry.dat;
+    buildInputs = [ elmPackages.elm elmPackages.elm-test ];
+    buildPhase = pkgs.elmPackages.fetchElmDeps {
+      elmPackages = import ./nix/elm-srcs.nix;
+      elmVersion = "0.19.1";
+      registryDat = ./nix/registry.dat;
+    };
+
+    doCheck = true;
+    checkPhase = ''
+      env ELM_HOME=.elm elm-test
+    '';
+
+    installPhase = ''
+      make dist
+      mkdir -p $out/share/
+      mv dist $out/share/elo-anything
+    '';
   };
-
-  doCheck = true;
-  checkPhase = ''
-    env ELM_HOME=.elm elm-test
-  '';
-
-  installPhase = ''
-    make dist
-    mkdir -p $out/share/
-    mv dist $out/share/elo-anything
-  '';
 }
