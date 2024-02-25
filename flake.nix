626f1514106049c8ef9212679f65877b2e95123d
Brian Hicks <brian@brianthicks.com>
2024-02-25 13:56:12 -0600

    update to release-23.11

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs.url = "github:NixOS/nixpkgs/release-21.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/release-23.11";
   };
 
   outputs = { self, nixpkgs, flake-utils }:
